f61fae9460a992b765a28f44f0e7fba5e907bf25
Robin Stephenson <robin.mascros@gmail.com>
2016-02-07 22:55:28 +0000

    Create models for User, Party, and joining table

--- playlistr/spotify_playlists/models.py
+++ playlistr/spotify_playlists/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.CharField(max_length=64, primary_key=True)
+    spotify_email = models.EmailField()
+    access_token = models.CharField(max_length=200)
+    refresh_token = models.CharField(max_length=200)
+    access_token_expiry = models.DateTimeField()
+
+
+class UserPartyJoin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    party = models.ForeignKey(Party, on_delete=models.SET_NULL, null=True)
+
+
+class Party(models.Model):
+    id = models.AutoField(primary_key=True)
+    target_duration_time = models.PositiveIntegerField()
+    last_used = models.DateTimeField()
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL)
