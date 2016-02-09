a4dbdcfd8c89c6d0c72428169b3ba98f71704767
Robin Stephenson <robin.mascros@gmail.com>
2016-02-09 18:24:56 +0000

    Register spotify_playlists in settings

--- playlistr/playlistr/settings.py
+++ playlistr/playlistr/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'spotify_playlists',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
