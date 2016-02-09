38103888ade5cfb8cfc2460621e5e629d1bb3f08
Robin Stephenson <robin.mascros@gmail.com>
2016-02-09 18:28:18 +0000

    Created index view

--- playlistr/spotify_playlists/views.py
+++ playlistr/spotify_playlists/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from spotify_playlists.services import API
+from os import path
 
-# Create your views here.
+module_dir = path.dirname(__file__)
+file_path = path.join(module_dir, "secret.txt")
+f = open(file_path, "r")
+client_secret = f.readline()
+api = API(
+    "38c7aa7c8b0a4172aa46a5b7833b8454",
+    client_secret,
+    "user-read-private user-read-email",
+    "http://127.0.0.1:8000/redirect"
+)
+
+
+def index(request):
+    context = {
+        "spotify_auth_url": api.get_auth_request_url()
+    }
+    return render(request, "spotify_playlists/index.html", context=context)
+
+
