4a6bc800ce5582bd725dc704812a8505979053c5
Robin Stephenson <robin.mascros@gmail.com>
2016-02-09 18:25:53 +0000

    Re ordered models to fix undefined error

--- playlistr/spotify_playlists/models.py
+++ playlistr/spotify_playlists/models.py
@@ -16,13 +16,13 @@ class User(models.Model):
             return True
 
 
-class UserPartyJoin(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    party = models.ForeignKey(Party, on_delete=models.SET_NULL, null=True)
-
-
 class Party(models.Model):
     id = models.AutoField(primary_key=True)
     target_duration_time = models.PositiveIntegerField()
     last_used = models.DateTimeField()
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class UserPartyJoin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    party = models.ForeignKey(Party, on_delete=models.SET_NULL, null=True)
