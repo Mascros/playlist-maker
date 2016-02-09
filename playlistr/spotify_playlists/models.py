26f20ecbc1ca6dce9f7e605b65a47288c16f208d
Robin Stephenson <robin.mascros@gmail.com>
2016-02-09 19:45:15 +0000

    Removed prefixes from models

--- playlistr/spotify_playlists/models.py
+++ playlistr/spotify_playlists/models.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 
 class User(models.Model):
-    _id = models.CharField(max_length=64, primary_key=True)
-    _spotify_email = models.EmailField()
-    _access_token = models.CharField(max_length=200)
-    _refresh_token = models.CharField(max_length=200)
-    _token_expiry = models.DateTimeField()
+    id = models.CharField(max_length=64, primary_key=True)
+    spotify_email = models.EmailField()
+    access_token = models.CharField(max_length=200)
+    refresh_token = models.CharField(max_length=200)
+    token_expiry = models.DateTimeField()
 
     def token_expired(self):
         if datetime.now() < self.access_token_expiry:
@@ -17,10 +17,10 @@ class User(models.Model):
 
 
 class Party(models.Model):
-    _id = models.AutoField(primary_key=True)
-    _target_duration_time = models.PositiveIntegerField()
-    _last_used = models.DateTimeField()
-    _creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    id = models.AutoField(primary_key=True)
+    target_duration_time = models.PositiveIntegerField()
+    last_used = models.DateTimeField()
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 
 class UserPartyJoin(models.Model):
