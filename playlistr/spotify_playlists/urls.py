30f38df7d969212156e04f2bbea279ff7c02255c
Robin Stephenson <robin.mascros@gmail.com>
2016-02-11 19:36:04 +0000

    Register start and join party urls

--- playlistr/spotify_playlists/urls.py
+++ playlistr/spotify_playlists/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'spotify_playlists'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^redirect$', views.redirect, name='redirect')
+    url(r'^redirect$', views.redirect, name='redirect'),
+    url(r'^join$', views.join, name='join'),
+    url(r'^start$', views.start, name='start')
 ]
