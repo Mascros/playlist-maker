b85eceec297aca7331d5b5f202feeed4ce83ccc6
Robin Stephenson <robin.mascros@gmail.com>
2016-02-13 18:17:33 +0000

    Created start view and placeholders for join and save_party
    Placeholder so the server will actually start

--- playlistr/spotify_playlists/views.py
+++ playlistr/spotify_playlists/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from spotify_playlists.services import API
 from os import path
 from spotify_playlists.models import User
+from django.utils.crypto import get_random_string
+import logging
 
 module_dir = path.dirname(__file__)
 file_path = path.join(module_dir, "secret.txt")
@@ -14,6 +16,8 @@ api = API(
     "user-read-private user-read-email",
     "http://127.0.0.1:8000/redirect"
 )
+
+log = logging.getLogger(__name__)
 
 
 def index(request):
@@ -32,3 +36,28 @@ def redirect(request):
     u.save()
     request.session['id'] = u.id
     return render(request, 'spotify_playlists/logged_in.html')
+
+
+def join(request):
+    return HttpResponse("Not implemented yet")
+
+
+def start(request):
+    try:
+        user_id = request.session['id']
+        party_name = request.POST.get('party_name')
+        party_id = get_random_string(length=8)
+        log.debug("User_id '{}' starting party with name '{}' and party_id '{}' ".format(user_id, party_name, party_id))
+        context = {
+            "party_name": party_name,
+            "party_id": party_id
+        }
+        return render(request, 'spotify_playlists/start_party.html', context=context)
+
+    except KeyError:
+        log.debug("User with no id in session, redirected to index")
+        return render(request, 'spotify_playlists/index.html')
+
+
+def save_party(request):
+    return HttpResponse("Not implemented yet")
