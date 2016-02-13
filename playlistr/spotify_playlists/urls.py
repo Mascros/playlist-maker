ceb8954fa1545e80299b62ce311e67df4c10858b
Robin Stephenson <robin.mascros@gmail.com>
2016-02-13 18:18:05 +0000

    Registered urls for start party and save party

--- playlistr/spotify_playlists/urls.py
+++ playlistr/spotify_playlists/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^redirect$', views.redirect, name='redirect'),
     url(r'^join$', views.join, name='join'),
-    url(r'^start$', views.start, name='start')
+    url(r'^start$', views.start, name='start'),
+    url(r'^save-party$', views.save_party, name='save_party')
 ]
