905c05b7d75644e05737a5c5006039a6b5164d84
Robin Stephenson <robin.mascros@gmail.com>
2016-02-18 18:15:45 +0000

    Create redirect view tests for when the user is logged in

--- playlistr/spotify_playlists/tests.py
+++ playlistr/spotify_playlists/tests.py
@@ -3,8 +3,10 @@ from django.test import TestCase, Client
 from django.test.utils import setup_test_environment
 from django.core.exceptions import FieldError
 from django.core.urlresolvers import reverse
+from unittest.mock import MagicMock
 
 from .models import User
+from .services import API
 
 setup_test_environment()
 client = Client()
@@ -52,3 +54,47 @@ class IndexViewTests(TestCase):
         """
         self.assertIn(b'https://accounts.spotify.com/authorize?', self.res.content)
         self.assertIn(b'response_type=code', self.res.content)
+
+    def tearDown(self):
+        self.res.close()
+
+
+class RedirectViewLoggedInTests(TestCase):
+    def setUp(self):
+        # Prevent save() from crashing because we don't have the side effects from the methods we stub
+        User.save = MagicMock()
+
+        user = {
+            'access_token': "token"
+        }
+        API.get_login_tokens = MagicMock(return_value=user)
+
+        profile = {
+            'email': 'test@example.com',
+            'id': 'test_user_id'
+        }
+        API.get_current_user_profile = MagicMock(return_value=profile)
+        self.res = client.get(reverse('spotify_playlists:redirect'))
+
+    def test_status_code(self):
+        """
+        the redirect view should return a 200 OK when the user is Logged in
+        """
+        self.assertEqual(self.res.status_code, 200)
+
+    def test_user_session(self):
+        """
+        the redirect view should create a session for the user with their id stored
+        """
+        self.assertEqual(client.session['id'], 'test_user_id')
+
+    def test_user_saved(self):
+        """
+        the redirect view should save the user to the database
+        """
+        # The comment below stops PyCharm from complaining because it doesnt know setUp is always called first
+        # noinspection PyUnresolvedReferences
+        User.save.assert_called_with()
+
+    def tearDown(self):
+        self.res.close()
