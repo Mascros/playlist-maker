fc8e770d495b3e67d15b7d30093862463d5f501e
Robin Stephenson <robin.mascros@gmail.com>
2016-02-24 16:07:45 +0000

    Close secret.txt after reading

--- playlistr/spotify_playlists/views.py
+++ playlistr/spotify_playlists/views.py
@@ -12,6 +12,7 @@ module_dir = path.dirname(__file__)
 file_path = path.join(module_dir, "secret.txt")
 f = open(file_path, "r")
 client_secret = f.readline()
+f.close()
 api = API(
     "38c7aa7c8b0a4172aa46a5b7833b8454",
     client_secret,
