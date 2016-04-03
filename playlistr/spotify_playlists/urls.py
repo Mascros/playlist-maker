2ae2cca13b2e8abe3421584983e9155d524faafc
Robin Stephenson <robin.mascros@gmail.com>
2016-04-03 16:46:48 +0100

    Changed name of redirect view to redirecter to fix clash with django method name

--- playlistr/spotify_playlists/urls.py
+++ playlistr/spotify_playlists/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'spotify_playlists'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^redirect$', views.redirect, name='redirect'),
+    url(r'^redirect$', views.redirecter, name='redirect'),
     url(r'^join$', views.join, name='join'),
     url(r'^start$', views.start, name='start'),
     url(r'^save-party$', views.save_party, name='save_party'),
