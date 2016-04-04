dbf03c1f10717d440bcaea02d2b4592be63709d9
Robin Stephenson <robin.mascros@gmail.com>
2016-04-04 13:25:05 +0100

    url for the publish button

--- playlistr/spotify_playlists/urls.py
+++ playlistr/spotify_playlists/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^join$', views.join, name='join'),
     url(r'^start$', views.start, name='start'),
     url(r'^save-party$', views.save_party, name='save_party'),
-    url(r'^testing/session', views.testing_session, name='testing_session')
+    url(r'^testing/session$', views.testing_session, name='testing_session'),
+    url(r'^publish$', views.publish, name='publish')
 ]
