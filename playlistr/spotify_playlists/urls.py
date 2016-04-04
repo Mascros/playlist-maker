c3c9088a843294e47f540cfe2000edb7ffcddabc
Robin Stephenson <robin.mascros@gmail.com>
2016-04-04 16:42:04 +0100

    Removed some trailing / from urls where they shouldn't be

--- playlistr/spotify_playlists/urls.py
+++ playlistr/spotify_playlists/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'spotify_playlists'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^redirect/$', views.redirecter, name='redirect'),
+    url(r'^redirect$', views.redirecter, name='redirect'),
     url(r'^join/(?P<party_id>[A-Za-z0-9]{8})$', views.join, name='join'),
-    url(r'^join/$', views.join, name='join'),
+    url(r'^join$', views.join, name='join'),
     url(r'^start/$', views.start, name='start'),
     url(r'^save-party/$', views.save_party, name='save_party'),
     url(r'^testing/session/$', views.testing_session, name='testing_session'),
