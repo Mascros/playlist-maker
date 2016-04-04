4e4c6231b643a1ca18bb427a66c88814ba7303d7
Robin Stephenson <robin.mascros@gmail.com>
2016-04-04 13:35:36 +0100

    Added trailing slashes to all urls

--- playlistr/spotify_playlists/urls.py
+++ playlistr/spotify_playlists/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'spotify_playlists'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^redirect$', views.redirecter, name='redirect'),
-    url(r'^join$', views.join, name='join'),
-    url(r'^start$', views.start, name='start'),
-    url(r'^save-party$', views.save_party, name='save_party'),
-    url(r'^testing/session$', views.testing_session, name='testing_session'),
-    url(r'^publish$', views.publish, name='publish')
+    url(r'^redirect/$', views.redirecter, name='redirect'),
+    url(r'^join/p$', views.join, name='join'),
+    url(r'^start/$', views.start, name='start'),
+    url(r'^save-party/$', views.save_party, name='save_party'),
+    url(r'^testing/session/$', views.testing_session, name='testing_session'),
+    url(r'^publish/$', views.publish, name='publish')
 ]
