4c0d52b06d155e2a2bb1941197a15b0936021ca4
Robin Stephenson <robin.mascros@gmail.com>
2016-06-29 17:10:59 +0100

    Starting again to fix major arc and design problems

--- playlistr/spotify_playlists/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpotifyPlaylistsConfig(AppConfig):
-    name = 'spotify_playlists'
--- tests.sh
+++ tests.sh
@@ -1,3 +1,2 @@
 cd playlistr
-echo "0123456789abcdef" > spotify_playlists/secret.txt
 python manage.py test
