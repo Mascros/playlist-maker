cd8006e6edf9cca2cefa8f9c44d68acf0d59b2c6
Robin Stephenson <robin.mascros@gmail.com>
2016-09-13 17:07:14 +0100

    created tests for the selector class

---
+++ playlistr/publisher/test_selector.py
@@ -0,0 +1,70 @@
+from unittest import TestCase
+from publisher.selector import Selector
+from publisher.selectionlibrary import SelectionLibrary
+from publisher.selectiontrack import SelectionTrack
+
+tracks = []
+for i in range(6):
+    track = SelectionTrack.get_testing_instance(
+        'album{}'.format(i),
+        'artist{}'.format(i),
+        'song{}'.format(i),
+        i
+    )
+    tracks.append(track)
+libraries = (
+    tracks[0:4],
+    tracks[2:6]
+)
+request = {
+    'target_no_songs': 3,
+    'libraries': libraries
+}
+
+
+class TestSelector(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.selector = Selector(SelectionLibrary, SelectionTrack)
+        cls.tracks = cls.selector.choose_tracks(request)
+
+    def test_init_invalid_library(self):
+        with self.assertRaises(TypeError):
+            Selector(SelectionTrack, SelectionTrack)
+
+    def test_init_invalid_track(self):
+        with self.assertRaises(TypeError):
+            Selector(SelectionLibrary, SelectionLibrary)
+
+    def test_choose_tracks_invalid_req_type(self):
+        """should raise a TypeError if the request is not a dict"""
+        with self.assertRaises(TypeError):
+            self.selector.choose_tracks("")
+
+    def test_choose_tracks_invalid_req_values(self):
+        bad_request = {
+            "name": 12
+        }
+        with self.assertRaises(ValueError):
+            self.selector.choose_tracks(bad_request)
+
+    def test_choose_tracks_return_type(self):
+        """should return a list of strings"""
+        self.assertIs(type(self.tracks), list)
+        self.assertIs(type(self.tracks[0]), str)
+
+    def test_choose_tracks_subset(self):
+        """should return a subset of all members tracks"""
+        universal = set(libraries[0] + libraries[1])
+        for track in self.tracks:
+            if track not in universal:
+                self.fail()
+
+    def test_choose_tracks_no_duplicates(self):
+        """returned tracks should not invclude any duplicates"""
+        seen = []
+        for track in self.tracks:
+            if track in seen:
+                self.fail()
+            else:
+                seen.append(track)
